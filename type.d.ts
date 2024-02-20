571337400c70cb945c20608e5ca75c18fa568a0e
eddichen <eddi@eddichen.com>
2024-02-20 16:52:24 +0000

    Adding types for venues

--- type.d.ts
+++ type.d.ts
@@ -2,3 +2,13 @@ type Users = {
   id: number;
   name: string;
 };
+
+interface Venue {
+  name: string | undefined;
+  lat: number | undefined;
+  lng: number | undefined;
+}
+
+interface VenueWithResidency extends Venue {
+  residency: string | null;
+}
