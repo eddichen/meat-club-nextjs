1cca48bed769d3b2219ba327eada12dc97cc3e3b
eddichen <eddi@eddichen.com>
2024-02-20 16:54:50 +0000

    Updating the venue field to use the autocomplete service

--- components/venue-field.tsx
+++ components/venue-field.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
   AdvancedMarker,
   Map,
@@ -8,80 +8,125 @@ import {
   useMapsLibrary,
 } from "@vis.gl/react-google-maps";
 import { Autocomplete, TextField, Box } from "@mui/material";
-import { debounce } from "@mui/material/utils";
-import { HandleFormProps } from "./add-event-form";
+import { HandleVenueProps } from "./add-event-form";
+import MapHandler from "./map-handler";
+
+interface VenueFieldProps {
+  handleVenue: ({ name, venue }: HandleVenueProps) => void;
+}
 
 const getOptions = (
-  venues: google.maps.places.PlaceResult[] | null,
+  results: google.maps.places.AutocompletePrediction[] | null,
 ): (string | undefined)[] => {
-  if (!venues) return [];
-  return venues.map((venue) => venue.name);
+  if (!results) return [];
+  return results.map((result) => result.description);
 };
 
-export default function VenueField({
-  handleVenue,
-}: {
-  handleVenue: ({ name, venue }: HandleFormProps) => void;
-}) {
-  const [placesService, setPlacesService] =
-    useState<google.maps.places.PlacesService | null>(null);
-  const [venueOptions, setVenueOptions] = useState<
-    google.maps.places.PlaceResult[] | null
-  >(null);
-  const [searchValue, setSearchValue] = useState("");
-
-  const getPlaces = useMemo(
-    () =>
-      debounce(
-        (
-          placesService: google.maps.places.PlacesService,
-          searchValue: string,
-        ) => {
-          placesService.nearbySearch(
-            {
-              bounds: new google.maps.LatLngBounds(
-                { lat: 51.3446573, lng: -0.491017 },
-                { lat: 51.694998, lng: 0.2361655 },
-              ),
-              keyword: searchValue,
-            },
-            (results, status) => {
-              if (status === google.maps.places.PlacesServiceStatus.OK) {
-                setVenueOptions(results);
-              }
-            },
-          );
-        },
-        400,
-      ),
-    [],
-  );
-
+export default function VenueField({ handleVenue }: VenueFieldProps) {
   const map = useMap();
   const placesLibrary = useMapsLibrary("places");
 
-  useEffect(() => {
-    if (!placesLibrary || !map) return undefined;
+  const [placesService, setPlacesService] =
+    useState<google.maps.places.PlacesService | null>(null);
+  const [sessionToken, setSessionToken] =
+    useState<google.maps.places.AutocompleteSessionToken>();
+  const [autocompleteService, setAutocompleteService] =
+    useState<google.maps.places.AutocompleteService | null>(null);
+  const [predictionResults, setPredictionResults] = useState<
+    Array<google.maps.places.AutocompletePrediction>
+  >([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedPlace, setSelectedPlace] =
+    useState<google.maps.places.PlaceResult | null>(null);
+  const [fetchingData, setFetchingData] = useState<boolean>(false);
 
+  useEffect(() => {
+    if (!placesLibrary || !map) return;
+
+    setAutocompleteService(new placesLibrary.AutocompleteService());
     setPlacesService(new placesLibrary.PlacesService(map));
+    setSessionToken(new placesLibrary.AutocompleteSessionToken());
+
+    return () => setAutocompleteService(null);
   }, [placesLibrary, map]);
 
-  useEffect(() => {
-    if (!placesService) return;
+  const fetchPredictions = useCallback(
+    async (inputValue: string) => {
+      if (!autocompleteService || !inputValue) {
+        setPredictionResults([]);
+        return;
+      }
 
-    if (!searchValue) {
-      setVenueOptions([]);
-      return;
-    }
+      const request = {
+        input: inputValue,
+        sessionToken,
+        componentRestrictions: { country: "gb" },
+        fields: ["name", "geometry"],
+      };
+      const response = await autocompleteService.getPlacePredictions(request);
 
-    getPlaces(placesService, searchValue);
-    if (venueOptions && venueOptions.length > 0) {
-      handleVenue({
-        name: "venue",
-        venue: venueOptions?.find((venue) => venue.name === searchValue),
-      });
-    }
-  }, [getPlaces, placesService, searchValue, handleVenue, venueOptions]);
+      setPredictionResults(response.predictions);
+    },
+    [autocompleteService, sessionToken],
+  );
+
+  const onInputChange = useCallback(
+    (value: string) => {
+      console.log(value);
+      setSearchValue(value);
+      fetchPredictions(value);
+    },
+    [fetchPredictions],
+  );
+
+  const onSelect = useCallback(
+    (value: string) => {
+      if (!placesLibrary || !value) return;
+
+      const prediction = predictionResults.find(
+        (result) => result.description === value,
+      );
+
+      if (!prediction) return;
+
+      setFetchingData(true);
+
+      const detailRequestOptions = {
+        placeId: prediction.place_id,
+        fields: ["geometry", "name"],
+        sessionToken,
+      };
+
+      const detailsRequestCallback = (
+        placeDetails: google.maps.places.PlaceResult | null,
+      ) => {
+        setSelectedPlace(placeDetails);
+        setSearchValue(placeDetails?.name ?? "");
+        setSessionToken(new placesLibrary.AutocompleteSessionToken());
+
+        setFetchingData(false);
+
+        handleVenue({
+          name: "venue",
+          venue: {
+            name: placeDetails?.name,
+            lat: placeDetails?.geometry?.location?.lat(),
+            lng: placeDetails?.geometry?.location?.lng(),
+          },
+        });
+      };
+
+      placesService?.getDetails(detailRequestOptions, detailsRequestCallback);
+    },
+    [
+      setSelectedPlace,
+      placesLibrary,
+      placesService,
+      sessionToken,
+      predictionResults,
+      handleVenue,
+    ],
+  );
 
   return (
     <>
@@ -93,25 +138,28 @@ export default function VenueField({
           gestureHandling={"greedy"}
           disableDefaultUI={true}
         >
-          {venueOptions &&
-            venueOptions.map((venue) => (
-              <AdvancedMarker
-                key={venue.place_id}
-                position={{
-                  lat: venue.geometry?.location?.lat() || 0,
-                  lng: venue.geometry?.location?.lng() || 0,
-                }}
-                title={venue.name}
-              />
-            ))}
+          {selectedPlace && (
+            <AdvancedMarker
+              key={selectedPlace.place_id}
+              position={{
+                lat: selectedPlace.geometry?.location?.lat() || 0,
+                lng: selectedPlace.geometry?.location?.lng() || 0,
+              }}
+              title={selectedPlace.name}
+            />
+          )}
+          <MapHandler place={selectedPlace} />
         </Map>
       </Box>
       <Autocomplete
-        options={getOptions(venueOptions)}
+        options={getOptions(predictionResults)}
         freeSolo
         filterOptions={(x) => x}
         filterSelectedOptions
-        onInputChange={(event, inputValue) => setSearchValue(inputValue)}
+        onInputChange={(event: React.SyntheticEvent<Element, Event>, value) =>
+          onInputChange(value)
+        }
+        onChange={(event, value) => onSelect(value as string)}
         renderInput={(params) => (
           <TextField {...params} label="Add a venue" name="venue" fullWidth />
         )}
