907a6bc8cb839ba871572702e5a7bdc2328f3294
eddichen <eddi@eddichen.com>
2024-03-03 17:12:06 +0000

    Adding a max width to the events page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,6 +2,7 @@ import EventsItem from "@/components/events-item";
 import { EventsMap } from "@/components/events-map";
 import EventsTable from "@/components/events-table";
 import { getEvents } from "@/lib/events";
+import { Container } from "@mui/material";
 
 export default async function Events() {
   const eventsData = await getEvents();
@@ -9,12 +10,12 @@ export default async function Events() {
   const { rows: events } = eventsData;
 
   return (
-    <>
+    <Container maxWidth="lg">
       <EventsMap events={events as MeatClubEvent[]} />
       <h1>Meat Club Events</h1>
       <EventsTable>
         <EventsItem events={events as MeatClubEvent[]} />
       </EventsTable>
-    </>
+    </Container>
   );
 }
