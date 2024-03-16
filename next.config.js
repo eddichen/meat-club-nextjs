9299d8677df5e9c20994bfc52e7ebba65a527d35
eddichen <eddi@eddichen.com>
2024-03-16 16:46:12 +0000

    Removing experiemental server actions as it is enabled by default in nextjs 14

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["images.ctfassets.net"],
-  },
-  experimental: {
-    serverActions: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
